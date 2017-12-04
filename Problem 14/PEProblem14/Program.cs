0badf4f7f23bede949f7f7a9b01242281e20ea18
MG Potgieter <mgandr@gmail.com>
2017-12-05 00:20:56 +0200

    Problem 14

---
+++ Problem 14/PEProblem14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEProblem14 {
+    class Program {
+        static void Main(string[] args) {
+
+            double longestLenght = 0;
+            double startingNumber = 2;
+            for (double i = 13; i < 1000000; i++) {
+                double count = 0;
+                double val = i;
+                while (val > 1) {
+                    if (val % 2 == 0) {
+                        val /= 2;
+                    }
+                    else {
+                        val = 3 * val + 1;
+                    }
+                    count++;
+                }
+                if (count+1 > longestLenght) {
+                    startingNumber = i;
+                    longestLenght = count;
+                }
+            }
+            Console.WriteLine(startingNumber);
+            Console.ReadKey();
+
+        }
+    }
+}
