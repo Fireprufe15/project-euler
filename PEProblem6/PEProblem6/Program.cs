b10b2ac26c30722f81156f208dc9df9fa372d7df
MG Potgieter <mgandr@gmail.com>
2017-12-04 20:02:37 +0200

    problem 6

---
+++ PEProblem6/PEProblem6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEProblem6 {
+    class Program {
+        static void Main(string[] args) {
+
+            var sumOfSquares = 0;
+            var sum = 0;
+            for (int i = 1; i < 101; i++) {
+                sumOfSquares += i * i;
+                sum += i;
+            }
+            var answer = (sum * sum) - sumOfSquares;
+            Console.WriteLine(answer);
+            Console.ReadKey();
+
+        }
+    }
+}
