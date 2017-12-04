bc146b3a92198018a5d789e65020e4c1b37e1687
MG Potgieter <mgandr@gmail.com>
2017-12-04 22:51:43 +0200

    Problem 12

---
+++ Problem 12/PEProblem12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEProblem12 {
+    class Program {
+        static void Main(string[] args) {
+
+            int i = 1;
+            bool found = false;
+            while (!found) {
+                var trianglenumber = (i * (i + 1)) / 2;
+                var factors = new List<long>();
+                for (int factor = 1; factor <= Math.Sqrt(trianglenumber); factor++) {
+                    if (trianglenumber%factor == 0) {
+                        factors.Add(factor);
+                        if (factor != trianglenumber/factor) {
+                            factors.Add(trianglenumber / factor);
+                        }
+                    }
+                }
+                if (factors.Count >= 500) {
+                    found = true;
+                    Console.WriteLine(trianglenumber);
+                }
+                i++;
+
+            }
+            Console.ReadKey();
+            
+
+        }
+
+       
+    }
+}
