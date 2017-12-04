261b36b88624f09907aaa11c9b4103a6bc3c7305
MG Potgieter <mgandr@gmail.com>
2017-12-04 20:09:41 +0200

    problem 1 to 3

---
+++ Problem 2/PEProblem2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEProblem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 0;
+            var sum = 0;
+            Queue<int> fib = new Queue<int>();
+            while (num < 4000000)
+            {
+                if (num == 0)
+                {
+                    fib.Enqueue(0);
+                    fib.Enqueue(1);
+                }
+                num = fib.Dequeue() + fib.Peek();
+                if (num % 2 == 0) sum += num;
+                fib.Enqueue(num);
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Problem 3/PEProblem3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEProblem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 600851475143;
+
+            double i = 1;
+            double primeFactor = 0;
+            while (i < num/2)
+            {
+                if (num%i == 0)
+                {
+                    var prime = isPrime(i);
+                    
+                    if (prime) primeFactor = i;
+                    if (prime) Console.WriteLine(primeFactor);
+                }
+                i++;
+            }
+            Console.WriteLine(primeFactor);
+            Console.ReadKey();
+        }
+
+        static bool isPrime(double num)
+        {
+            if (num <= 1) return false;
+            else if (num <= 3 || num == 5) return true;
+            else if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0) return false;
+
+            int i = 6;
+            var boundary = (int)Math.Floor(Math.Sqrt(num));
+
+            while (i <= boundary)
+            {
+                if (num % (i + 1) == 0 || num % (i + 5) == 0) return false;
+                i += 6;
+            }
+
+            //for (int i = 5; i*i < num; i+=6)
+            //{
+            //    if (num % i == 0 || num % (i + 2) == 0) return false;
+            //}
+            return true;
+
+        }
+    }
+}
