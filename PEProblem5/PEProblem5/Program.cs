cac14cadfb83c20295abb9c91f7a6a10650ddd3a
MG Potgieter <mgandr@gmail.com>
2017-12-04 19:30:29 +0200

    problem 5

---
+++ PEProblem5/PEProblem5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEProblem5 {
+    class Program {
+        static void Main(string[] args) {
+
+            int cur = 0;
+            var found = false;
+            while (!found) {
+                cur++;
+                found = true;
+                for (int i = 1; i < 21; i++) {
+                    if (cur%i > 0) {
+                        found = false;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(cur);
+            Console.ReadKey();
+
+        }
+    }
+}
