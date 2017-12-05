0f86449c10df03691d771eac71aaeb7d80c1056e
MG Potgieter <mgandr@gmail.com>
2017-12-05 15:38:10 +0200

    Problem 16

---
+++ Problem 16/PEProblem16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEProblem16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger number = (BigInteger)Math.Pow(2, 1000);
+            string num = number.ToString();
+            BigInteger sum = 0;
+            foreach (var item in num)
+            {
+                sum += BigInteger.Parse(item.ToString());
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
